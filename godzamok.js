aaee53ae6d2a6fb9906da29eb3037f476200f35e
Shayne Keller <uskickbutt@gmail.com>
2018-07-14 16:02:28 -0700

    derped comments

--- godzamok.js
+++ godzamok.js
@@ -1,15 +1,15 @@
 var x = 5
 var y = function() { 
 	Game.ObjectsById[0].sell(x);
-	#Game.ObjectsById[0].sell(x);
-	#Game.ObjectsById[0].sell(x);
-	#Game.ObjectsById[0].sell(x);
-	#Game.ObjectsById[0].sell(x);
+	//Game.ObjectsById[0].sell(x);
+	//Game.ObjectsById[0].sell(x);
+	//Game.ObjectsById[0].sell(x);
+	//Game.ObjectsById[0].sell(x);
 	Game.ObjectsById[0].buy(x);
-	#Game.ObjectsById[0].buy(x);
-	#Game.ObjectsById[0].buy(x);
-	#Game.ObjectsById[0].buy(x);
-	#Game.ObjectsById[0].buy(x);
+	//Game.ObjectsById[0].buy(x);
+	//Game.ObjectsById[0].buy(x);
+	//Game.ObjectsById[0].buy(x);
+	//Game.ObjectsById[0].buy(x);
 	setTimeout(function(){ y() }, 10100);
 }
 y();
