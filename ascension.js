7a59d71f2b39067a00ceb226b1bf7ccca607adda
Shayne Keller <uskickbutt@gmail.com>
2018-07-15 17:07:44 -0700

    added initial pantheon codes

---
+++ ascension.js
@@ -0,0 +1,7 @@
+  var pantheon = Game.Objects["Temple"].minigame;
+  var godMazok = Game.Objects["Temple"].minigame.gods["ruin"];
+  var godMuridal = Game.Objects["Temple"].minigame.gods["labor"];
+
+  pantheon.slot[0]=godMazok.id;
+  pantheon.slot[1]=godMuridal.id;
+
