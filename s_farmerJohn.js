07ac4d23fb7f69bcac08198117374fe7f51e69ea
Shayne Keller <uskickbutt@gmail.com>
2018-08-05 19:03:38 -0700

    running main function before setting the interval.

--- s_farmerJohn.js
+++ s_farmerJohn.js
@@ -11,6 +11,7 @@ if (!farm.minigameLoaded && !farm.freeze) {
   alert("Farming not enabled yet (or frozen), buy/upgrade some farms or unfreeze!");
 } else {
   console.log("Congrats, you can farm shit now.");
+  farmAway();
   setInterval(farmAway, 1000 * 60 * 60 * 3);
 }
 
