de4bfb90935172f8780a52a2316cabdb7458b043
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 20:01:09 -0700

    removed debug message from godzamok

--- s_godzamok.js
+++ s_godzamok.js
@@ -4,7 +4,7 @@ function fireDragonMagic() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100*xOfferingsToGodzamok);
   if (FrozenCookies.autoBuy == 0) FrozenCookies.autoBuy = 1;
-  console.log("Destruction was had by all who opposed.");
+  //console.log("Destruction was had by all who opposed.");
 }
 
 function startTheFire() {
