6823d1daf7ce8462ff76679ab1c857f065ae70fd
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 19:43:09 -0700

    renamed godzamok functions

--- s_godzamok.js
+++ s_godzamok.js
@@ -1,17 +1,17 @@
 var intervalID;
 
-function runTheMagic() {
+function fireDragonMagic() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100*xOfferingsToGodzamok);
   if (FrozenCookies.autoBuy == 0) FrozenCookies.autoBuy = 1;
 }
 
-function start() {
-  intervalID = setInterval(runTheMagic(), 10100);
+function startTheFire() {
+  intervalID = setInterval(fireDragonMagic(), 10100);
 }
 
-function stop() {
+function stopTheFire() {
   clearInterval(intervalID);
 }
 
-start();
+startTheFire();
