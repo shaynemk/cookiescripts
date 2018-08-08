dbca5e47a9b163fc38e6721fee70f26963625a70
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 19:26:41 -0700

    another godzamok update

--- s_godzamok.js
+++ s_godzamok.js
@@ -1,5 +1,13 @@
-setInterval(function() {
+var intervalID;
+
+function start() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100*xOfferingsToGodzamok);
   if (FrozenCookies.autoBuy == 0) FrozenCookies.autoBuy = 1;
-}, 10100);
+}
+
+function stop() {
+  clearInterval(intervalID);
+}
+
+intervalID = setInterval(start(), 10100);
