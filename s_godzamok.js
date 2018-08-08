fe735fc792f53c262e5f9b08702452de37ec0c77
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 19:19:15 -0700

    updated godzamok script to auto enable FC's AutoBuy function, since we now rely on it.

--- s_godzamok.js
+++ s_godzamok.js
@@ -1,4 +1,5 @@
 setInterval(function() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100*xOfferingsToGodzamok);
+  if (FrozenCookies.autoBuy == 0) FrozenCookies.autoBuy = 1;
 }, 10100);
