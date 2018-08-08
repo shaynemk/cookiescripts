0355100faacadb2f31680adf4287eb204a94453b
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 19:28:36 -0700

    added ability to disable godzamok and restart it

--- s_godzamok.js
+++ s_godzamok.js
@@ -1,13 +1,17 @@
 var intervalID;
 
-function start() {
+function runTheMagic() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100*xOfferingsToGodzamok);
   if (FrozenCookies.autoBuy == 0) FrozenCookies.autoBuy = 1;
+}
+
+function start() {
+  intervalID = setInterval(runTheMagic(), 10100);
 }
 
 function stop() {
   clearInterval(intervalID);
 }
 
-intervalID = setInterval(start(), 10100);
+start();
