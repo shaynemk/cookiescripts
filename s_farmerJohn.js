8be65c96bba183cb6179ff76e4534376b5bc05c3
Shayne Keller <uskickbutt@gmail.com>
2018-08-07 18:02:23 -0700

    more updates to farmer

--- s_farmerJohn.js
+++ s_farmerJohn.js
@@ -7,6 +7,7 @@ var soilClay = farmM.soils["clay"];
 var currentTile;
 var currentPlant;
 var xDEBUG = true;
+var needFertilizer = false;
 
 function useClay() {
   farmM.soil = soilClay.id;
@@ -16,7 +17,7 @@ function useClay() {
 function farmAway() {
   //farmM.tools.harvestAll.func();
   //farmM.soil = soilFertilizer.id;
-  debugLog("Set soil type to fertilizer.");
+  //debugLog("Set soil type to fertilizer.");
   if (!plantThumbcorn.unlocked) {
     //plant bakers wheat in correct orientation to get thumbcorn
     //use wood chips if possible (>300)
@@ -28,31 +29,33 @@ function farmAway() {
     //use clay when mature
     farm.seedSelected = plantThumbcorn.id;
   }
+  needFertilizer = false;
   for (x=0;x<6;x++) {
     for (y=0;y<6;y++) {
-      //check if plot is unlocked
       if (farmM.isTileUnlocked(x,y)) { // check if the tile is unlocked
         currentTile = farmM.getTile(x,y);
         if (currentTile[0] >= 1) { // check if there is something there
           currentPlant = farmM.plantsById[currentTile[0]-1];
           if (currentTile[1] < 90 && currentPlant.id == farm.seedSelected) { // plant is not quite old enough...i think this is out of 100? not sure.
-            //setTimeout(farmAway, 1000 * 60 * 3);
             debugLog("Thumbcorn (" + x + "," + y + ") age is < 90, skipping.");
             continue;
-          } else /*if (currentPlant.id != farm.seedSelected)*/ { // plant is the same as seed currently selected
+          } else /*if (currentPlant.id != farm.seedSelected)*/ { // plant is old enough and/or not the same seed planted
             farmM.harvest(x,y);
-            farmM.useTool(farm.seedSelected,x,y);
-            debugLog("Plot (" + x + ", " + y + ") had " + currentPlant.name + " planted, planted: " + farmM.plantsById[currentTile[0]-1].name);
+            plant(x,y);
+            debugLog("Plot (" + x + ", " + y + ") had " + currentPlant.name + " planted");
           }
-       } else {
-          farmM.useTool(farm.seedSelected,x,y);
-          debugLog("Planted " + farmM.plantsById[farm.seedSelected].name + " in plot (" + x + ", " + y + ").");
-        }
+        } else plant(x,y); // plot was empty, plant something
       }
     }
   }
-  setTimeout(useClay, 1000 * 60 * 9); // 9 minutes = 3 ticks @ 3min/ticks (fertilizer) = thumbcorn maturation
+  if (needFertilizer) setTimeout(useClay, 1000 * 60 * 9); // 9 minutes = 3 ticks @ 3min/ticks (fertilizer) = thumbcorn maturation
   setInterval(farmAway, 1000 * 60 * 60 * 3); // run farm func again in 3 hours
+}
+
+function plant(x,y) {
+  farmM.useTool(farm.seedSelected,x,y);
+  needFertilizer = true;
+  debugLog("Planted " + farmM.plantsById[currentTile[0]-1].name + " in plot (" + x + ", " + y + ").");
 }
 
 function debugLog(message) {
