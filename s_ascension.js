37b74d99927f252df93c9b7788c459510b38d433
uskickbutt <uskickbutt@gmail.com>
2018-08-12 19:13:04 -0400

    update some things

--- s_ascension.js
+++ s_ascension.js
@@ -1,16 +1,16 @@
-var pantheon = Game.Objects["Temple"].minigame;
-var godMazok = Game.Objects["Temple"].minigame.gods["ruin"];
-var godMuridal = Game.Objects["Temple"].minigame.gods["labor"];
+var pantheon = Game.Objects.Temple.minigame;
+var godMazok = Game.Objects.Temple.minigame.gods.ruin;
+var godMuridal = Game.Objects.Temple.minigame.gods.labor;
 
 // check for chocolate egg
 if (!Game.Upgrades["Chocolate egg"].unlocked || !Game.Upgrades["Chocolate egg"].canBuy()) {
-  Game.Note("Auto Ascension Error", "Chocolate Egg not available to can't buy it, aborting.");
+  Game.Note("Auto Ascension Error", "Chocolate Egg either not available or can't buy it, aborting.");
   break fin;
 }
 
 // disable FC && godzamok scripts
-FrozenCookies.autoBuy = 0;
-stopTheFire();
+if (typeof FrozenCookies != "undefined") FrozenCookies.autoBuy = 0;
+if (typeof Destructo != "undefined") Destructo.stop();
 
 // sell everything
 /*for (var x = 0; x < Game.ObjectsById.length; x++) {
@@ -29,7 +29,7 @@ Game.Reincarnate (true);
 
 // enable FC & Godzamok scripts
 FrozenCookies.autoBuy = 1;
-startTheFire();
+Destructo.start();
 
 // set the gods in pantheon
 pantheon.slot[0]=godMazok.id;
