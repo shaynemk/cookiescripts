69bffbd58a56c8cd683a88c5f29af8852e9fb3c1
uskickbutt <uskickbutt@gmail.com>
2018-08-12 19:10:53 -0400

    renamed to Destructo

--- s_godzamok.js
+++ s_godzamok.js
@@ -1,19 +1,19 @@
-var Pete = {};
+var Destructo = {};
 
-Pete.magic = function() {
+Destructo.magic = function() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
   Game.ObjectsById[0].sell(100 * xOfferingsToGodzamok);
   
   if (typeof FrozenCookies != "undefined" && FrozenCookies.autoBuy === 0) FrozenCookies.autoBuy = 1;
 }
 
-Pete.start = function() {
-  Pete.magic();
-  Pete.intervalID = setInterval(Pete.magic, 10100);
+Destructo.start = function() {
+  Destructo.magic();
+  Destructo.intervalID = setInterval(Destructo.magic, 10100);
 }
 
-Pete.stop = function() {
-  clearInterval(Pete.intervalID);
+Destructo.stop = function() {
+  clearInterval(Destructo.intervalID);
 }
 
-Pete.start();
+Destructo.start();
