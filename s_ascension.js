377f55aa9625034762b5f57551b935f5ce4249e5
uskickbutt <uskickbutt@gmail.com>
2018-08-12 19:41:04 -0400

    fleshing out Angelic Ascension into full on script

--- s_ascension.js
+++ s_ascension.js
@@ -1,38 +1,78 @@
-var pantheon = Game.Objects.Temple.minigame;
-var godMazok = Game.Objects.Temple.minigame.gods.ruin;
-var godMuridal = Game.Objects.Temple.minigame.gods.labor;
+var AA = {};
 
-// check for chocolate egg
-if (!Game.Upgrades["Chocolate egg"].unlocked || !Game.Upgrades["Chocolate egg"].canBuy()) {
-  Game.Note("Auto Ascension Error", "Chocolate Egg either not available or can't buy it, aborting.");
-  break fin;
+AA.init = function() {
+  var pantheon = Game.Objects.Temple.minigame;
+  var godMazok = Game.Objects.Temple.minigame.gods.ruin;
+  var godMuridal = Game.Objects.Temple.minigame.gods.labor;
 }
 
-// disable FC && godzamok scripts
-if (typeof FrozenCookies != "undefined") FrozenCookies.autoBuy = 0;
-if (typeof Destructo != "undefined") Destructo.stop();
+AA.checkForEgg = function() {
+  // check for chocolate egg
+  return (!Game.Upgrades["Chocolate egg"].unlocked || !Game.Upgrades["Chocolate egg"].canBuy());
+}
 
-// sell everything
-/*for (var x = 0; x < Game.ObjectsById.length; x++) {
-  Game.ObjectsById[x].sell(Game.ObjectsById[x].amount);
-}*/
-Game.ObjectsById.foreach (function (b){
-  b.sell(-1);
-});
+AA.disableHelpers = function() {
+  // disable FC && godzamok scripts
+  if (typeof FrozenCookies != "undefined") FrozenCookies.autoBuy = 0;
+  if (typeof Destructo != "undefined") Destructo.stop();
+}
 
-// buy chocolate egg
-Game.Upgrades["Chocolate egg"].buy();
+AA.enableHelpers = function() {
+  // enable FC & Godzamok scripts
+  FrozenCookies.autoBuy = 1;
+  Destructo.start();
+}
 
-// ascend and return
-Game.Ascend(true);
-Game.Reincarnate (true);
+AA.sellAll = function() {
+  // sell everything
+  for (var x = 0; x < Game.ObjectsById.length; x++) {
+    Game.ObjectsById[x].sell(-1);
+  }
+  // Game.ObjectsById.foreach (function (b){
+  //   b.sell(-1);
+  // });
+}
 
-// enable FC & Godzamok scripts
-FrozenCookies.autoBuy = 1;
-Destructo.start();
+AA.buyChocEgg = function() {
+  // buy chocolate egg
+  Game.Upgrades["Chocolate egg"].buy();
+}
 
-// set the gods in pantheon
-pantheon.slot[0]=godMazok.id;
-pantheon.slot[1]=godMuridal.id;
+AA.upAndBack= function() {
+  // ascend and return
+  Game.Ascend(true);
+  Game.Reincarnate (true);
+}
 
-fin:
+AA.setIdols = function() {
+  // set the gods in pantheon
+  pantheon.slot[0]=godMazok.id;
+  pantheon.slot[1]=godMuridal.id;
+}
+
+AA.buyAllUpgrades = function() {
+  Game.storeBuyAll();
+}
+
+AA.ascend = function() {
+  if (!AA.checkForEgg) Game.Note("Angelic Ascension Error", "Chocolate Egg either not available or can't buy it, aborting.");
+  else {
+    AA.init();
+    
+    AA.disableHelpers();
+    
+    AA.sellAll();
+    
+    AA.buyChocEgg();
+    
+    AA.upAndBack();
+    
+    AA.enableHelpers();
+    
+    AA.setIdols();
+    
+    setTimeout(clearInterval(setInterval(AA.buyAllUpgrades,5),1000 * 20)); // buy all upgrades every 5 seconds for 20 seconds
+  }
+}
+
+Game.Note("Angelic Ascension","Angelic Ascension has loaded. Run 'AA.ascend();' in the console to ascend.");
