c4762676bde8c968c07a975ecd4064c1910bae19
uskickbutt <uskickbutt@gmail.com>
2018-08-19 14:13:03 -0400

    removed dependency on FC for buying cursors back

--- s_godzamok.js
+++ s_godzamok.js
@@ -2,9 +2,9 @@ var Destructo = {};
 
 Destructo.magic = function() {
   var xOfferingsToGodzamok = 6; //how many hundreds to sell/buy
-  Game.ObjectsById[0].sell(100 * xOfferingsToGodzamok);
-  
-  if (typeof FrozenCookies != "undefined" && FrozenCookies.autoBuy === 0) FrozenCookies.autoBuy = 1;
+  Game.Objects.Cursor.sell(100 * xOfferingsToGodzamok);
+  Game.Objects.Cursor.buy(100 * xOfferingsToGodzamok);
+  //if (typeof FrozenCookies != "undefined" && FrozenCookies.autoBuy === 0) FrozenCookies.autoBuy = 1;
 }
 
 Destructo.start = function() {
